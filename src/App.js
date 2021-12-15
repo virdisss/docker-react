fff9db89b9a0097f33ef0a262fa018efb635d8db
Virdis Antenor <virdis.antenor@outlook.com>
2021-12-14 22:58:09 -0400

    Changed app text

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Change on the feature branch!</p>
       </header>
     </div>
   );
